580a0952c7bb47eeab1d386e6644dc2bf0c9bd35
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-11-25 03:54:42 +0600

    get data from jobs

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xmhoqrm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const jobsCollection = client.db('workSphere').collection('jobs');
+        const bidsCollection = client.db('workSphere').collection('bids');
+
+        // get all jobs data from db 
+        app.get('/jobs', async (req, res) => {
+            const result = await jobsCollection.find().toArray();
+            res.send(result)
+        })
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', async (req, res) => {
+    res.send('Hello from workSphere Server...')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on Port: ${port}`);
+})
